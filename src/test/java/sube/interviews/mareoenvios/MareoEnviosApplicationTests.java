2b3d44cab65a9e4d89fb6ba6d84a76a252f1d8fd
Federico Martinez Leiva <fedemartinezleiva@gmail.com>
2026-06-29 17:18:47 -0300

    add test environment and clean context

--- src/test/java/sube/interviews/mareoenvios/MareoEnviosApplicationTests.java
+++ src/test/java/sube/interviews/mareoenvios/MareoEnviosApplicationTests.java
@@ -2,8 +2,12 @@ package sube.interviews.mareoenvios;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class MareoEnviosApplicationTests {
 
 	@Test
